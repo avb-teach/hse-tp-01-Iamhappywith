b55476c7983d086f86633189fc35de7bf09edca3
Iamhappywith <135255831+Iamhappywith@users.noreply.github.com>
2025-04-29 00:52:23 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -29,8 +29,28 @@ copy_files() {
         done
         
         total[$filename]=1
-        cp "$file" "$target/$filename"
+
+        relative_path="${file#$source/}"
+        relative_dir=$(dirname "$relative_path")
+        mkdir -p "$target/$relative_dir"
+        
+        cp "$file" "$target/$relative_path"
     done < <(find "$source" -type f -print0)
+    
+    if [ -n "$max_depth" ]; then
+        while IFS= read -r -d '' dir; do
+            [ "$dir" = "$source" ] && continue
+            
+            ap=$(dirname "$dir")
+            current_depth=$(echo "$dir" | tr -cd '/' | wc -c)
+            current_depth=$((current_depth - base_depth))
+            
+            if [ "$current_depth" -lt "$max_depth" ]; then
+                relative_path="${dir#$source/}"
+                mkdir -p "$target/$relative_path"
+            fi
+        done < <(find "$source" -type d -print0)
+    fi
 }
 
 declare -A total
@@ -38,6 +58,12 @@ declare -A reps
 
 source="$1"
 target="$2"
-max_depth="$4"
+
+max_depth=""
+for arg in "$@"; do
+    if [[ "$arg" == "-=max_depth"* ]]; then
+        max_depth="${arg#*=}"
+    fi
+done
 
 copy_files "$source" "$target" "$max_depth"
