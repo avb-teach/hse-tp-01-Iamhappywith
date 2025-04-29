675ab4049ce2909983da02c55bf2fd429ab941d8
Iamhappywith <135255831+Iamhappywith@users.noreply.github.com>
2025-04-29 13:43:31 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -63,7 +63,11 @@ max_depth=""
 for arg in "$@"; do
     if [[ "$arg" == "-=max_depth"* ]]; then
         max_depth="${arg#*=}"
+        if [ -n "$max_depth" ]; then
+            max_depth=$((max_depth - 1))
+        fi
     fi
 done
+
 
 copy_files "$source" "$target" "$max_depth"
